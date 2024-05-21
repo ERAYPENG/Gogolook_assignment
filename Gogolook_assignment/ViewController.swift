b1a17d7b4b500f6a73c9a58574dceeb90430e9be
ERAYPENG <jameslebron23@hotmail.com.tw>
2024-05-21 11:28:43 +0800

    Initial Commit

---
+++ Gogolook_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gogolook_assignment
+//
+//  Created by ERAY on 2024/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
